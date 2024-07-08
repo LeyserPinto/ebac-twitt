741c40dd71d58659644f257802011d7ed7bfdee7
Leyser Pinto <59316466+LeyserPinto@users.noreply.github.com>
2024-07-07 23:29:04 -0300

    Update axios.ts

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000', // Update this to your Django backend URL
+    baseURL: 'http://leyserp.pythonanywhere.com/', // Update this to your Django backend URL
 });
 
 
